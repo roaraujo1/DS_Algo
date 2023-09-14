b1a2676f5a760cde17359b1fb539c058479e603f
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-09-14 10:27:54 -0700

    added the tree folder and the heap file

---
+++ Trees/heap.py
@@ -0,0 +1,9 @@
+class TreeNode:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+    #create tree
+    def createTree(self, arr: list) ->None:
+        pass
