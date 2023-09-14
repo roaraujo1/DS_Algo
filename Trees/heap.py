2acc1ac25937ae7733901ede747926c431c1b483
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-09-14 12:08:30 -0700

    updated code

--- Trees/heap.py
+++ Trees/heap.py
@@ -3,7 +3,28 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
-    
-    #create tree
-    def createTree(self, arr: list) ->None:
-        pass
+
+class Heap:
+    def __init__(self):
+        self.root = None
+
+
+    #insertNode function that checks where to add the node
+    def insertNode(self,newNode:int)->None:
+        #case 1: there is no root node
+        if not self.root:
+            self.root = TreeNode(newNode)
+        #case 2: root exists and now recursion needs to take place to see where to add node
+        
+        
+
+heap = Heap()
+heap.insertNode(3)
+heap.insertNode(4)
+heap.insertNode(5)
+heap.createTree()
+
+        
+
+
+
