0246e4fb8b1f0119a0a168f0d98e43faf61fb5a8
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-09-16 11:50:26 -0700

    traversals

--- Trees/binaryTree.py
+++ Trees/binaryTree.py
@@ -18,23 +18,56 @@ class Tree:
             root.right = self.insertNode(root.right,elemToInsert)
         return root
     
+    #root,left,right
     def preOrder(self,root:TreeNode)->list:
         res = []
         def dfs(node):
             if not node:
                 return 
             res.append(node.val)
-            dfs(root.left)
-            dfs(root.right)
+            dfs(node.left)
+            dfs(node.right)
         dfs(root)
-        return res 
+        return res
+    
+    #left,root,right
+    def inOrder(self,root:TreeNode)->list:
+        res = []
+        def dfs(node):
+            if not node:
+                return 
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return res
+    
+    #left,right,root
+    def postOrder(self,root:TreeNode)->list:
+        res = []
+        def dfs(node):
+            if not node:
+                return 
+            dfs(node.left)
+            dfs(node.right)
+            res.append(node.val)
+        dfs(root)
+        return res
+        
+
+
+
 
        
         
 tree = Tree()
-tree.insertNode(2)
-tree.insertNode(3)
-tree.insertNode(5)
+tree.root = tree.insertNode(tree.root,2)
+tree.root = tree.insertNode(tree.root,1)
+tree.root = tree.insertNode(tree.root,5)
+tree.root = tree.insertNode(tree.root,0)
 print(tree.preOrder(tree.root))
+print(tree.inOrder(tree.root))
+print(tree.postOrder(tree.root))
+
 
 
