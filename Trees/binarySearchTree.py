bdb8b1181cb49ce0c1a7ad75c03651353ee866b0
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-09-21 12:02:18 -0700

    Starting BST

---
+++ Trees/binarySearchTree.py
@@ -0,0 +1,11 @@
+class TreeNode:
+    def __init__(self,val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        
